06c79bb16c79f7554ae424f4fbfd81e3aeae4ebe
EricRinger6 <eric.ringer6@gmail.com>
2020-11-01 22:10:53 -0500

    Fixing

--- src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Console.java
@@ -1,15 +1,20 @@
 package com.zipcodewilmington.scientificcalculator;
+
 import java.util.Scanner;
+
 /**
- * Updated by Gunjan on 10/31/2020
+ * Updated by Gunjan / Eric / Munir on 10/31/2020
  */
 public class Console {
+
     public static void print(String output, Object... args) {
         System.out.printf(output, args);
     }
+
     public static void println(String output, Object... args) {
         print(output + "\n", args);
     }
+
     public static String getStringInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
         println(prompt);
@@ -18,27 +23,53 @@ public class Console {
     }
     // This method checks if the entered value is 'Int' , If not requests user to key in another value.
     public static Integer getIntegerInput(String prompt) {
+
         Scanner scanner = new Scanner(System.in);
         Integer userInput = 0;
         println(prompt);
+
+
         if (scanner.hasNextInt()) {
             userInput = scanner.nextInt();
         } else
         {
             userInput = Console.getIntegerInput ("Not a Number value, Please re-enter:");
+
         }
+
         return userInput;
+
+
     }
-    // This method checks if the entered value is 'Double' , If not requests user to key in another value.
+// This method checks if the entered value is 'Double' , If not requests user to key in another value.
+
     public static Double getDoubleInput(String prompt) {
         Scanner scanner = new Scanner(System.in);
+
         Double userInputDouble = 0D;
-        println(prompt);
+        print(prompt,userInputDouble);
+        //println(userInputDouble);
+
         if (scanner.hasNextDouble()) {
             userInputDouble = scanner.nextDouble();
         } else
         {
             userInputDouble = Console.getDoubleInput ("Not a Number value, Please re-enter:");
+
         }
+
         return userInputDouble;
-    }}
+    }
+
+    public static  String getOperationInput(String prompt)
+    {
+        Scanner operation = new Scanner(System.in);
+        println (prompt);
+        String ops = operation.nextLine();
+        return ops;
+    }
+
+
+
+
+}
